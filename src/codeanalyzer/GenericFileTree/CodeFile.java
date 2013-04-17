287d7cfdacb26d4f7c205e0f677604ea8a5fa036
Crstn <Crstn@192.168.1.36>
2013-04-17 10:08:58 +0100

    Composite for Files createTreeDirectory()

---
+++ src/codeanalyzer/GenericFileTree/CodeFile.java
@@ -0,0 +1,19 @@
+package codeanalyzer.GenericFileTree;
+
+public class CodeFile extends GenericFile {
+
+    public CodeFile(String path) {
+        super(path);
+    }
+
+    @Override
+    public Boolean isDirectory() {
+        return false;
+    }
+
+    @Override
+    public Boolean isCodeFile() {
+        return true;
+    }
+    
+}
