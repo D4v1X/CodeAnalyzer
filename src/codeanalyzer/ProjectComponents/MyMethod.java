d14915632474015f652b039889ea66873a5fd8d1
Roman <Roman@10.230.56.40>
2013-04-25 11:34:24 +0100

    Class MyMethod semi-completed

--- src/codeanalyzer/ProjectComponents/MyMethod.java
+++ src/codeanalyzer/ProjectComponents/MyMethod.java
@@ -1,7 +1,12 @@
 package codeanalyzer.ProjectComponents;
 
+import codeanalyzer.ProjectComponents.MetricClass.CodeLines;
+
 public class MyMethod {
-    Integer cyclomaticComplexity;
+    private String name;
+    private Integer cyclomaticComplexity;
+    private Integer parameters;
+    private CodeLines codeLines;
 
     public void setCyclomaticComplexity(Integer cyclomaticComplexity) {
         this.cyclomaticComplexity = cyclomaticComplexity;
